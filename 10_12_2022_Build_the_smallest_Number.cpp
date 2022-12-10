3a07931660968d73e83f265c094a8e79737b67af
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-12-10 13:29:23 +0530

    Update 10_12_2022_Build_the_smallest_Number.cpp
    https://practice.geeksforgeeks.org/problems/build-the-smallest2841/1

--- 10_12_2022_Build_the_smallest_Number.cpp
+++ 10_12_2022_Build_the_smallest_Number.cpp
@@ -1,73 +1,44 @@
+//Stack Problem   Time: O(N)  Space: O(N) 
 
 string buildLowestNumber(string s, int k){
     
     int n = s.size();
-    if(n==k) return "0";
 
+    if(k>=n) return "0";
+    if(k==0) return s;
+
+    string ans = "";
     stack<char> st;
     st.push(s[0]);
 
-    int i = 1;
-
-    while(k!=0 and i<n){
-
-        if(st.empty()){
-            if(s[i]=='0'){
-                i++;
-            }
-            else{
-                st.push(s[i]);
-                i++;
-            }
+    for(int i=1; i<n; i++){
+        while(k!=0 and !st.empty() and s[i]<st.top()){
+            st.pop();
+            k--;
         }
 
-        else if(s[i]>st.top()){
-            st.push(s[i]);
-            i++;
-        }
-
-        else{
-            while(!st.empty() and (s[i]<st.top()) and (k!=0)){
-                st.pop();
-                k--;
-            }
-            
-            if(s[i]=='0' and !st.empty()){
-                st.push(s[i]);
-            }
-            if(s[i]!='0'){
-                st.push(s[i]);
-            }
-            
-            i++;
-        }
-    }
-
-    while(i<n){
         st.push(s[i]);
-        i++;
-    }
-    
-    int l = st.size();
-    
-    //Important condition. 
-    if(k>=l) return "0";
-    
-    string ans(l, ' ');
 
-    int j = 0;
-    while(!st.empty()){
-        ans[j] = st.top();
+        //Removing Trailing Zeros
+        if(st.size()==1 and st.top()=='0'){
+            st.pop();
+        }
+    }
+
+    //Removing Ending Values --> Increasing
+    while(k!=0 and !st.empty()){
+        k--;
         st.pop();
-        j++;
+    }
+
+    //Storing Values in String 
+    while(!st.empty()){
+        ans.push_back(st.top());
+        st.pop();
     }
 
     reverse(ans.begin(), ans.end());
+    if(ans.size()==0) return "0";
 
-    j = 0;
-    while(ans[j]=='0'){
-        j++;
-    }
-    
-    return ans.substr(j, l);
+    return ans;
 }
