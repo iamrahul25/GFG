3845d9ba54e403f7f7557b6bea20d153f895f97a
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-12-10 12:39:45 +0530

    Create 10_12_2022_Build_the_smallest_Number.cpp
    https://practice.geeksforgeeks.org/problems/build-the-smallest2841/1

---
+++ 10_12_2022_Build_the_smallest_Number.cpp
@@ -0,0 +1,73 @@
+
+string buildLowestNumber(string s, int k){
+    
+    int n = s.size();
+    if(n==k) return 0;
+
+    stack<char> st;
+    st.push(s[0]);
+
+    int i = 1;
+
+    while(k!=0 and i<n){
+
+        if(st.empty()){
+            if(s[i]=='0'){
+                i++;
+            }
+            else{
+                st.push(s[i]);
+                i++;
+            }
+        }
+
+        else if(s[i]>st.top()){
+            st.push(s[i]);
+            i++;
+        }
+
+        else{
+            while(!st.empty() and (s[i]<st.top()) and (k!=0)){
+                st.pop();
+                k--;
+            }
+            
+            if(s[i]=='0' and !st.empty()){
+                st.push(s[i]);
+            }
+            if(s[i]!='0'){
+                st.push(s[i]);
+            }
+            
+            i++;
+        }
+    }
+
+    while(i<n){
+        st.push(s[i]);
+        i++;
+    }
+    
+    int l = st.size();
+    
+    //Important condition. 
+    if(k>=l) return "0";
+    
+    string ans(l, ' ');
+
+    int j = 0;
+    while(!st.empty()){
+        ans[j] = st.top();
+        st.pop();
+        j++;
+    }
+
+    reverse(ans.begin(), ans.end());
+
+    j = 0;
+    while(ans[j]=='0'){
+        j++;
+    }
+    
+    return ans.substr(j, l);
+}
