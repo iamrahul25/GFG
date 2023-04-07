546db05596d956e832baea9e83ed65138d98635b
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2023-04-07 09:51:03 +0530

    Update 07_04_2023_Add_Minimum_Characters_to_make_Palindrome.cpp
    https://practice.geeksforgeeks.org/problems/55dbfdc246f3f62d6a7bcee7664cacf6be345527/1

--- April 2023/07_04_2023_Add_Minimum_Characters_to_make_Palindrome.cpp
+++ April 2023/07_04_2023_Add_Minimum_Characters_to_make_Palindrome.cpp
@@ -1,24 +1,46 @@
 //Link: https://practice.geeksforgeeks.org/problems/55dbfdc246f3f62d6a7bcee7664cacf6be345527/1
-
 class Solution{   
 public:
-    //Two Pointer Approach: 
+
+    //Brute Force Approach: Time: O(N^2) Space: O(1) --> All Test Case Passed!
     int addMinChar(string str){    
         int n = str.size();
-        int i = 0;
-        int j = n-1;
-        int ans = 0;
         
-        while(i<=j){
-            if(str[i]==str[j]){
-                i++; j--;
-            }
-            else{
-                ans++;
-                i = 0;
-                j = n - ans - 1;
-            }
+        for(int i=0; i<n; i++){
+            if(isPalindrome(str,0,n-i-1)) return i;
         }
-        return ans;
+        
+        return n-1;
     }
+    
+    bool isPalindrome(string &s, int i, int j){
+        while(i<=j){
+            if(s[i]!=s[j]) return false;
+            i++; j--;
+        }
+        return true;
+    }
+
+
+    //Two Pointer Approach: Time: O(N^2) Space: O(1) --> All Test Case Passed!
+    // int addMinChar(string str){    
+    //     int n = str.size();
+    //     int i = 0;
+    //     int j = n-1;
+        
+    //     int ans = 0;
+        
+    //     while(i<=j){
+    //         if(str[i]==str[j]){
+    //             i++; j--;
+    //         }
+    //         else{
+    //             ans++;
+    //             i = 0;
+    //             j = n - ans - 1;
+    //         }
+    //     }
+        
+    //     return ans;
+    // }
 };
