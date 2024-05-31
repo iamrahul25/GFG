ff02d45ffd898369ac7e48e1c317ac6e92acf1ff
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2024-05-31 10:47:55 +0530

    Create swap-two-nibbles.cpp
    https://www.geeksforgeeks.org/problems/swap-two-nibbles-in-a-byte0446/1

---
+++ April 2024/swap-two-nibbles.cpp
@@ -0,0 +1,13 @@
+//Link: https://www.geeksforgeeks.org/problems/swap-two-nibbles-in-a-byte0446/1
+
+class Solution {
+  public:
+    int swapNibbles(int n) {
+        string s = bitset<8>(n).to_string();
+        string s1 = s.substr(0, 4);
+        string s2 = s.substr(4);
+        s = s2 + s1;
+
+        return stoi(s,0,2);
+    }
+};
